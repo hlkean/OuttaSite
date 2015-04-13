304af73495b6b6761068804d7a15087fd1f79be6
hlkean <hlkean@gmail.com>
2015-04-13 13:34:44 -0400

    added magnefic.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     
+    
     var now = new Date();
     var hrs = now.getHours();
     var msg = "";
@@ -12,4 +13,54 @@ $(document).ready(function() {
 
     $('.two').append(msg);
     
+
+
+//Begin Magnific JS
+    
+    
+     $('#resume').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+    
+    $('#persona').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+
+$("#video").magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+ });
+    
+$("#edge").magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+    $('#frogg').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
 });
